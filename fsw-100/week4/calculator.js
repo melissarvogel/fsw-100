7c41f7a26db9b52245c6257ba2178f80deb5f741
Melissarvogel <melissa.vogel@bryanuniversity.edu>
2020-05-09 14:14:48 -0400

    first commit

---
+++ fsw-100/week4/calculator.js
@@ -0,0 +1,20 @@
+const readline = require("readline-sync")
+
+var x = 2
+var y = 3
+console.log(x + y)
+
+var x = 2
+var y = 3
+console.log(x * y)
+
+var x = 2
+var y = 6
+console.log(x / y)
+
+var x = 2
+var y = 3
+console.log(x - y)
+
+
+
